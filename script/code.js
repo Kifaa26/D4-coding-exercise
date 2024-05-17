382ce93d7b221ed5b96bdfc217c8a3dc5eb4f6d7
Kifaa26 <akifahpalekar@gmail.com>
2024-05-17 13:37:00 +0200

    object

---
+++ script/code.js
@@ -0,0 +1,17 @@
+let person1 = {
+    name : "John",
+    age : 30,
+    city : "New York"
+}
+
+let person2 = {
+    name : "June",
+    age : 26,
+    city : "London"
+};
+
+console.log(person1);
+
+let people = [person1, person2];
+
+console.log(people); 
